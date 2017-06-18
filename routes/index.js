56f4a449ac43e7ffae7e65cf869f2756848ee6fd
Sabit Huraira <sabitzhabit@gmail.com>
2017-06-18 10:58:28 +0700

    load data from file

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var menus = require('..//models/menu.json');
+var contents= require('..//models/content.json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("masuk /")
-  res.render('index', { title: 'Farifam' });
+  res.render('index/index', { menus: menus });
+});
+
+router.get('/json', function(req, res, next) {
+  const content = contents.find(c=>{
+    return c.id == "json"
+  })
+  res.render('index/page', { content: content });
+});
+
+router.get('/json_detail', function(req, res, next) {
+  const content = contents.find(c=>{
+    return c.id == "json_detail"
+  })
+  res.render('index/page', { content: content });
 });
 
 module.exports = router;
