0c0a55c5072ac2464c066b18d53290a9fb5e971e
Sabit Huraira <sabitzhabit@gmail.com>
2017-06-19 23:30:29 +0700

    mongo, list and insert data

--- routes/mongo.js
+++ routes/mongo.js
@@ -5,7 +5,7 @@ var Customer = require('../models/mongo_customer');
 
 router.get('/', function(req, res, next) {
   Customer.find(function(err, docs){
-      //console.log(docs);
+      console.log(docs);
       res.render('mongo/index', { datas: docs });
   });
 });
@@ -15,8 +15,6 @@ router.get('/new', function(req, res, next) {
 });
 
 router.post('/new', function(req, res, next) {
-  console.log(req.body);
-  
   var customer_instance = new Customer({ 
     name: req.body.name,
     email: req.body.email,
@@ -25,13 +23,57 @@ router.post('/new', function(req, res, next) {
   
   customer_instance.save(function (err) {
     if (err) {
-      console.log(err);
+      return res.redirect('/mongo/new')
     } else {
-      console.log('meow');
+      return res.redirect('/mongo')
     }
   });
 
-  res.render('mongo/new', {});
+  //res.render('mongo/new', {});
+});
+
+router.get('/edit/:id', function(req, res, next) {
+  Customer.findOne({'_id': req.params.id},function(err, doc){
+      console.log(doc);
+      res.render('mongo/edit', { data: doc });
+  });
+});
+
+router.put('/edit/:id', function(req, res, next) {
+  console.log('enter put edit')
+  var customer_instance = new Customer({ 
+    name: req.body.name,
+    email: req.body.email,
+    address: req.body.address
+  });
+  
+  Customer.update({_id: req.params.id}, req.body , {upsert: true}, function (err, result) {
+    if (err) {
+      console.log(err)
+      return res.redirect('/mongo/edit/'+data)
+    } else {
+      return res.redirect('/mongo')
+    }
+  });
+});
+
+
+router.delete('/delete/:id', function(req, res, next) {
+  console.log('enter delete post')
+  var customer_instance = new Customer({ 
+    name: req.body.name,
+    email: req.body.email,
+    address: req.body.address
+  });
+  
+  Customer.update({_id: req.params.id}, req.body , function (err, result) {
+    if (err) {
+      console.log(err)
+      return res.redirect('/mongo/edit/'+data)
+    } else {
+      return res.redirect('/mongo')
+    }
+  });
 });
 
 module.exports = router;
